12372b4167d29665e1060ced0d9aa16c37fab08c
apminaei <amir_peiravi@yahoo.com>
2019-03-02 15:01:45 +0100

    Delete CTestTestfile.cmake

--- build-cmake/dune/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amir/Dune-projects/dune-howto-adaptive-grid-multilevel-quad/dune
-# Build directory: /home/amir/Dune-projects/dune-howto-adaptive-grid-multilevel-quad/build-cmake/dune
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("howto-adaptive-grid-multilevel-quad")
